29c4fdce34c2d7cec3a8578b909b144e1aed8f3e
Johannes von Rotz <jr@vrtz.ch>
2020-05-17 10:23:48 +0200

    Cleanup

--- tls.c
+++ tls.c
@@ -38,77 +38,71 @@
 #include "tls.h"
 #include "gemini.h"
 
-int create_socket(int port)
-{
-    int s;
-    struct sockaddr_in addr;
+int create_socket(int port) {
+	int s;
+	struct sockaddr_in addr;
+	
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s < 0) {
+		perror("Unable to create socket");
+		exit(EXIT_FAILURE);
+	}
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    s = socket(AF_INET, SOCK_STREAM, 0);
-    if (s < 0) {
-	perror("Unable to create socket");
-	exit(EXIT_FAILURE);
-    }
-
-    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-	perror("Unable to bind");
-	exit(EXIT_FAILURE);
-    }
-
-    if (listen(s, 1) < 0) {
-	perror("Unable to listen");
-	exit(EXIT_FAILURE);
-    }
-
-    return s;
+	if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+		perror("Unable to bind");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (listen(s, 1) < 0) {
+		perror("Unable to listen");
+		exit(EXIT_FAILURE);
+	}
+	
+	return s;
+}	
+	
+void init_openssl() {
+	SSL_load_error_strings();	
+	OpenSSL_add_ssl_algorithms();
 }
 
-void init_openssl()
-{ 
-    SSL_load_error_strings();	
-    OpenSSL_add_ssl_algorithms();
+void cleanup_openssl() {
+	EVP_cleanup();
 }
 
-void cleanup_openssl()
-{
-    EVP_cleanup();
-}
-
-SSL_CTX *create_context()
-{
-    const SSL_METHOD *method;
-    SSL_CTX *ctx;
+SSL_CTX *create_context() {
+	const SSL_METHOD *method;
+	SSL_CTX *ctx;
 #ifdef TLS_USE_V1_2_METHOD
-    method = TLSv1_2_server_method();
+	method = TLSv1_2_server_method();
 #else
-    method = TLS_server_method();
+	method = TLS_server_method();
 #endif /*TLS_USE_V1_2_METHOD*/
 
-    ctx = SSL_CTX_new(method);
-    if (!ctx) {
-	perror("Unable to create SSL context");
-	ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
-    }
+	ctx = SSL_CTX_new(method);
+	if (!ctx) {
+		perror("Unable to create SSL context");
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
 
-    return ctx;
+	return ctx;
 }
 
-void configure_context(SSL_CTX *ctx)
-{
+void configure_context(SSL_CTX *ctx) {
     SSL_CTX_set_ecdh_auto(ctx, 1);
 
-    /* Set the key and cert */
-    if (SSL_CTX_use_certificate_file(ctx, ssl_public_path, SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
-    }
+	if (SSL_CTX_use_certificate_file(ctx, ssl_public_path, SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, ssl_private_path, SSL_FILETYPE_PEM) <= 0 ) {
-        ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
-    }
+	if (SSL_CTX_use_PrivateKey_file(ctx, ssl_private_path, SSL_FILETYPE_PEM) <= 0 ) {
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
 }
